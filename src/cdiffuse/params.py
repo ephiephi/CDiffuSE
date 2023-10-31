ac40662710913ec5f73bf8379ecf435377febe63
ephiephi <ephiephi@gmail.com>
2023-10-31 20:03:08 +0200

    change more papareters for large model

--- src/cdiffuse/params.py
+++ src/cdiffuse/params.py
@@ -50,10 +50,12 @@ params = AttrDict(
     crop_mel_frames=62,  
 
     # Model params
-    residual_layers=30,
-    # residual_channels=64,
+    #residual_layers=30,
+    #residual_channels=64,
     residual_channels=128,
     dilation_cycle_length=10,
-    noise_schedule=np.linspace(1e-4, 0.035, 50).tolist(),
-    inference_noise_schedule=[0.0001, 0.001, 0.01, 0.05, 0.2, 0.35],
+    noise_schedule=np.linspace(1e-4, 0.02, 200).tolist(),
+    inference_noise_schedule=[0.0001, 0.001, 0.01, 0.05, 0.2, 0.7],
+    #noise_schedule=np.linspace(1e-4, 0.035, 50).tolist(),
+    #inference_noise_schedule=[0.0001, 0.001, 0.01, 0.05, 0.2, 0.35],
 )
